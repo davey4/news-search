ef20d0c7a5a341a6c7cf81e06d970fa08114ae19
David Martel <daveymartel@gmail.com>
2020-10-20 16:11:29 -0400

    refactored sports to oop

--- sports.js
+++ sports.js
@@ -4,43 +4,69 @@ const URL_ALL_LEAGES =
   "https://www.thesportsdb.com/api/v1/json/1/all_leagues.php";
 const URL_LOOKUP_LEAGUE = `https://www.thesportsdb.com/api/v1/json/1/lookupleague.php?id=${NFL_ID}`;
 
+const section = document.querySelector("section");
+const main = document.querySelector("main");
+
 const getNflGames = async () => {
   const URL_LOOKUP_NFL_EVENTS = `https://www.thesportsdb.com/api/v1/json/1/eventsnextleague.php?id=${NFL_ID}`;
   try {
     let response = await axios.get(URL_LOOKUP_NFL_EVENTS);
     listNflGames(response.data.events);
-    // console.log(response.data.events)
   } catch (error) {
     console.log(error);
   }
 };
 
-const listNflGames = (data) => {
-  let display = document.querySelector("#nfl-event-display");
-  removeElements(document.querySelectorAll(".teamDisplay"));
-  removeElements(document.querySelectorAll('.teamSched'))
-  let events = document.createElement("div");
-  events.className = "events";
-  events.innerText = "Upcoming NFL Games";
-  data.forEach((element) => {
+class Game {
+  constructor(event, date, time, description, pic) {
+    this.event = event;
+    this.date = date;
+    this.time = time;
+    this.description = description;
+    this.pic = pic;
+  }
+  displayGames() {
+    let pic = this.pic;
+    let events = document.createElement("div");
     let event = document.createElement("div");
     let time = document.createElement("div");
     let description = document.createElement("div");
     description.className = "description";
     event.className = "nflEvent";
-    event.style.backgroundImage = `url(${element.strThumb})`;
+    event.style.backgroundImage = `url(${pic})`;
 
-    event.innerText = element.strEvent;
-    time.innerText = `Date: ${element.dateEvent} Kickoff: ${element.strTime}`;
-    description.innerText = element.strDescriptionEN;
+    event.innerText = this.event;
+    time.innerText = `Date: ${this.date} Kickoff: ${this.time}`;
+    description.innerText = this.description;
 
     event.appendChild(time);
     event.appendChild(description);
     events.appendChild(event);
+    section.appendChild(events);
+  }
+}
+
+const listNflGames = (data) => {
+  removeElements(document.querySelectorAll(".banner"));
+  removeElements(document.querySelectorAll(".nflEvent"));
+  let teamDisplay = document.createElement("div");
+  teamDisplay.className = "banner";
+
+  teamDisplay.style.backgroundImage = `url(https://wallpapercave.com/wp/heeVWtl.jpg)`;
+
+  main.appendChild(teamDisplay);
+
+  data.forEach((element) => {
+    let games = new Game(
+      element.strEvent,
+      element.dateEvent,
+      element.strTime,
+      element.strDescriptionEN,
+      element.strThumb
+    );
+    games.displayGames();
   });
-  display.appendChild(events);
 };
-// getNflGames();
 
 const getNflTeams = async () => {
   const LIST_ALL_NFL_TEAMS = `https://www.thesportsdb.com/api/v1/json/1/lookup_all_teams.php?id=${NFL_ID}`;
@@ -78,16 +104,15 @@ const nflTeamPage = async (e) => {
 };
 
 const displayTeamInfo = (data) => {
-  removeElements(document.querySelectorAll(".events"));
-  let display = document.querySelector("#nfl-team-info");
-  removeElements(document.querySelectorAll(".teamDisplay"));
+  removeElements(document.querySelectorAll(".nflEvent"));
+  removeElements(document.querySelectorAll(".banner"));
   let teamDisplay = document.createElement("div");
-  teamDisplay.className = "teamDisplay";
+  teamDisplay.className = "banner";
   let banner = data[0].strTeamBanner;
 
   teamDisplay.style.backgroundImage = `url(${banner})`;
 
-  display.appendChild(teamDisplay);
+  main.appendChild(teamDisplay);
 };
 
 const getTeamSched = async (e) => {
@@ -102,28 +127,16 @@ const getTeamSched = async (e) => {
 };
 
 const displayTeamSched = (data) => {
-  console.log(data);
-  let display = document.querySelector("#nfl-team-info");
-  removeElements(document.querySelectorAll('.teamSched'))
-  let teamSched = document.createElement('div')
-  teamSched.className = 'teamSched'
-    data.forEach((element) =>{
-    let event = document.createElement("div");
-    let time = document.createElement("div");
-    let description = document.createElement("div");
-    description.className = "description";
-    event.className = "nflEvent";
-    event.style.backgroundImage = `url(${element.strThumb})`;
-
-    event.innerText = element.strEvent;
-    time.innerText = `Date: ${element.dateEvent} Kickoff: ${element.strTime}`;
-    description.innerText = element.strDescriptionEN;
-
-    event.appendChild(time);
-    event.appendChild(description);
-    teamSched.appendChild(event);
-  })
-  display.appendChild(teamSched)
+  data.forEach((element) => {
+    let games = new Game(
+      element.strEvent,
+      element.dateEvent,
+      element.strTime,
+      element.strDescriptionEN,
+      element.strThumb
+    );
+    games.displayGames();
+  });
 };
 
 const removeElements = (elms) => elms.forEach((el) => el.remove());
